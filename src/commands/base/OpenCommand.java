9e33cbb0c6e8a612b98723c60268e5170fbef725
AAhmedov05 <72496798+AAhmedov05@users.noreply.github.com>
2026-05-03 19:36:20 +0300

    OpenCommand check for .json extension

--- src/commands/base/OpenCommand.java
+++ src/commands/base/OpenCommand.java
@@ -13,9 +13,11 @@ public class OpenCommand extends Command {
     @Override
     public String execute(String[] params) {
         if (params.length < 2) return this.getDescription();
+        if (!params[1].toLowerCase().endsWith(".json"))  return "File must have .json extension.";
         try {
             session.openFile(params[1]);
-            return "File opened successfully.";
+            return "Successfully opened "+session.getFilePath()
+                    .substring(session.getFilePath().lastIndexOf('\\')+1);
         } catch (IOException e) {
             return "Failed to open file: " + e.getMessage();
         }
